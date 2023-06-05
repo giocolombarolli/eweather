538fafd309586c366aa4f4100f0f7d944d14b0b8
Giovani Colombarolli <giozeira@gmail.com>
2023-06-05 20:24:22 -0300

    Development (#3)
    * Insert fragment_main.xml, nav_graph.xml.
    
    * Project structure.
    
    * Project structure refactor.

---
+++ app/src/main/java/br/com/eweather/data/repository/WeatherRepository.kt
@@ -0,0 +1,4 @@
+package br.com.eweather.data.repository
+
+class WeatherRepository {
+}
--- app/src/main/java/br/com/eweather/ui/view/home/MainActivity.kt
+++ app/src/main/java/br/com/eweather/ui/view/home/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.giocolombarolli.eweather
+package br.com.eweather.ui.view.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.giocolombarolli.eweather.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
